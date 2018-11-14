469f6b0762ca1b07ce736f95b294ab6c8549173d
wind315 <lq314@qq.com>
2018-11-14 15:57:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@
 
 cd /v2ray
 wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-unzip v2ray.zip
+unzip v2ray.zip -d /v2ray/v2ray-v$VER-linux-64
 echo show current dir
 pwd
 ls -lR
@@ -16,8 +16,9 @@ mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
 mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
 mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
 ls -l
+rm -fR /v2ray/v2ray-v$VER-linux-64
 unzip -h
-ls v2ray
+ls -lR /
 
 chmod +x v2ray v2ctl
 sed -i "s/your_uuid/$UUID/g" config.json
